71a1f456bdb4edf0ab44661d11d58e616b4b225e
motherfather <father@mail.com>
2016-11-28 09:33:03 +0900

    수정

--- app/src/main/java/com/bit2016/dialogtest/MainActivity.java
+++ app/src/main/java/com/bit2016/dialogtest/MainActivity.java
@@ -1,7 +1,9 @@
 package com.bit2016.dialogtest;
 
+import android.app.Dialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButtonClick(View view) {
+        Dialog dialog = new Dialog(this);
+
+        dialog.setContentView(R.layout.dialog_test);
+        dialog.setTitle("Dialog Test");
+        dialog.show();
     }
 
 }
